5b5029059e048aa7a40f53ba5c088af07ba2b131
Nielsen Pierce <nielsenpi@softcat.com>
2022-09-02 13:18:18 +0100

    Update provider to use latest for tests

--- tests/advanced/provider.tf
+++ tests/advanced/provider.tf
@@ -13,7 +13,6 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
     }
 
   }
--- tests/basic/provider.tf
+++ tests/basic/provider.tf
@@ -12,7 +12,6 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
     }
 
   }
