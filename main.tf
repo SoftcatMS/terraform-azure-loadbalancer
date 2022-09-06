a58f45c1609413b766a06aed9bfd580aff79332c
Nielsen Pierce <nielsenpi@softcat.com>
2022-09-06 13:17:08 +0100

    Remove resource Group parameter and update backend_address_pool_ids name

--- main.tf
+++ main.tf
@@ -64,14 +64,13 @@ resource "azurerm_lb_probe" "azlb" {
 resource "azurerm_lb_rule" "azlb" {
   count                          = length(var.lb_port)
   name                           = element(keys(var.lb_port), count.index)
-  resource_group_name            = data.azurerm_resource_group.azlb.name
   loadbalancer_id                = azurerm_lb.azlb.id
   protocol                       = element(var.lb_port[element(keys(var.lb_port), count.index)], 1)
   frontend_port                  = element(var.lb_port[element(keys(var.lb_port), count.index)], 0)
   backend_port                   = element(var.lb_port[element(keys(var.lb_port), count.index)], 2)
   frontend_ip_configuration_name = var.frontend_name
   enable_floating_ip             = false
-  backend_address_pool_id        = azurerm_lb_backend_address_pool.azlb.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.azlb.id]
   idle_timeout_in_minutes        = 5
   probe_id                       = element(azurerm_lb_probe.azlb.*.id, count.index)
 }
