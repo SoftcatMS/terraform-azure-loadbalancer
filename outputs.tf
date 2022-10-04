3d0b4562116eb44934afa5211b1b3a2e92222014
Nielsen Pierce <nielsenpi@softcat.com>
2022-10-04 13:10:47 +0100

    Public IP created outside of module & enable Outbound NAT

--- outputs.tf
+++ outputs.tf
@@ -23,19 +23,19 @@ output "azurerm_lb_probe_ids" {
   value       = azurerm_lb_probe.azlb.*.id
 }
 
-output "azurerm_lb_nat_rule_ids" {
-  description = "the ids for the azurerm_lb_nat_rule resources"
-  value       = azurerm_lb_nat_rule.azlb.*.id
-}
+# output "azurerm_lb_nat_rule_ids" {
+#   description = "the ids for the azurerm_lb_nat_rule resources"
+#   value       = azurerm_lb_nat_rule.azlb.*.id
+# }
 
 output "azurerm_public_ip_id" {
   description = "the id for the azurerm_lb_public_ip resource"
-  value       = azurerm_public_ip.azlb.*.id
+  value       = data.azurerm_public_ip.azlb.*.id
 }
 
 output "azurerm_public_ip_address" {
   description = "the ip address for the azurerm_lb_public_ip resource"
-  value       = azurerm_public_ip.azlb.*.ip_address
+  value       = data.azurerm_public_ip.azlb.*.ip_address
 }
 
 output "azurerm_lb_backend_address_pool_id" {
